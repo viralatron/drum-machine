94ac551f7bbdb32e14e85058da5f45f567a454dc
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2022-12-27 17:02:59 -0300

    uploaded dist

--- README.md
+++
@@ -1,2 +0,0 @@
-# drum-machine
-Um reprodutor de samples feito como tarefa para o curso de bibliotecas FrontEnd do FreeCodeCamp
--- script.js
+++ script.js
@@ -1,102 +1,102 @@
-const Pads = [
-  {
-    key: "Q",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    title: "Heater 1"
-  },
-  {
-    key: "W",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-    title: "Heater 2"
-  },
-  {
-    key: "E",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-    title: "Heater 3"
-  },
-  {
-    key: "A",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-    title: "Heater 4"
-  },
-  {
-    key: "S",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-    title: "Clap"
-  },
-  {
-    key: "D",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-    title: "Open-HH"
-  },
-  {
-    key: "Z",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-    title: "Kick-n'-Hat"
-  },
-  {
-    key: "X",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-    title: "Kick"
-  },
-  {
-    key: "C",
-    samp: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-    title: "Closed-HH"
-  }
-];
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const Pads = [
+{
+  key: "Q",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  title: "Heater 1" },
+
+{
+  key: "W",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  title: "Heater 2" },
+
+{
+  key: "E",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  title: "Heater 3" },
+
+{
+  key: "A",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  title: "Heater 4" },
+
+{
+  key: "S",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  title: "Clap" },
+
+{
+  key: "D",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  title: "Open-HH" },
+
+{
+  key: "Z",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  title: "Kick-n'-Hat" },
+
+{
+  key: "X",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  title: "Kick" },
+
+{
+  key: "C",
+  samp: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  title: "Closed-HH" }];
+
+
 
 class DrumMachine extends React.Component {
   constructor(props) {
-    super(props);
+    super(props);_defineProperty(this, "playAudio",
 
-    this.state = {
-      display: "DrumPad"
-    };
-  }
 
-  playAudio = (ev) => {
-    const audio = ev.target.querySelector("audio");
 
-    this.setState({
-      display: audio.dataset.title
-    });
 
-    audio.play();
-  };
-  playKey = (ev) => {
-    const key = ev.key.toUpperCase();
-    const pad = document.querySelector(`.drum-pad#${key}`);
-    if (pad) pad.click();
-  };
+
+
+    ev => {
+      const audio = ev.target.querySelector("audio");
+
+      this.setState({
+        display: audio.dataset.title });
+
+
+      audio.play();
+    });_defineProperty(this, "playKey",
+    ev => {
+      const key = ev.key.toUpperCase();
+      const pad = document.querySelector(`.drum-pad#${key}`);
+      if (pad) pad.click();
+    });this.state = { display: "DrumPad" };}
   render() {
     document.addEventListener("keydown", this.playKey);
-    return (
-      <div id="drum-machine">
-        <div id="pad-bank">
-          {Pads.map((pad) => (
-            <DrumPad
-              keypad={pad.key}
-              src={pad.samp}
-              handle={this.playAudio}
-              title={pad.title}
-            />
-          ))}
-        </div>
-        <div id="pad-controller">
-          <p id="display">{this.state.display}</p>
-        </div>
-      </div>
-    );
-  }
-}
+    return /*#__PURE__*/(
+      React.createElement("div", { id: "drum-machine" }, /*#__PURE__*/
+      React.createElement("div", { id: "pad-bank" },
+      Pads.map((pad) => /*#__PURE__*/
+      React.createElement(DrumPad, {
+        keypad: pad.key,
+        src: pad.samp,
+        handle: this.playAudio,
+        title: pad.title }))), /*#__PURE__*/
+
+
+
+      React.createElement("div", { id: "pad-controller" }, /*#__PURE__*/
+      React.createElement("p", { id: "display" }, this.state.display))));
+
+
+
+  }}
+
 
 const DrumPad = ({ keypad, src, handle, title }) => {
-  return (
-    <button type="button" className="drum-pad" id={keypad} onClick={handle}>
-      <audio className="clip" src={src} id={keypad} data-title={title} />
-      {keypad}
-    </button>
-  );
+  return /*#__PURE__*/(
+    React.createElement("button", { type: "button", className: "drum-pad", id: keypad, onClick: handle }, /*#__PURE__*/
+    React.createElement("audio", { className: "clip", src: src, id: keypad, "data-title": title }),
+    keypad));
+
+
 };
-ReactDOM.render(<DrumMachine />, document.querySelector(".app"));
+ReactDOM.render( /*#__PURE__*/React.createElement(DrumMachine, null), document.querySelector(".app"));
